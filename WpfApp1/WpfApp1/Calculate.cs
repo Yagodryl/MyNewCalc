8e5c304ce3daf21db2288eb9acb1eab9ae1adf86
Maks <maksgorbatuk6@gmail.com>
2018-05-17 22:07:00 +1200

    AddClass

---
+++ WpfApp1/WpfApp1/Calculate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Calculate
+    {
+        public float a { get; set; }
+        public float b { get; set; }
+        public char ch { get; set; }
+      public float Return_Res()
+        {
+            switch(ch)
+            {
+                case 1:
+
+                    break;
+            }
+            return 0;
+        }
+    }
+}
